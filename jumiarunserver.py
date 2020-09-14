b6b17b45a9b837c2eb7d06426f851ad69310a400
KOUADIO PHINEAS <32209043+Phincode@users.noreply.github.com>
2020-09-14 21:54:13 +0000

    Add files via upload

---
+++ jumiarunserver.py
@@ -0,0 +1,15 @@
+﻿"""
+This script runs the HydraWeb application using a development server.
+"""
+
+from os import environ
+from jumiaApi import app
+
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', '0.0.0.0')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5400'))
+    except ValueError:
+        PORT = 5400
+    app.run(HOST, PORT)
+    #app.run(host='68.66.205.156', port=5400, debug=True)
